caab7af655063426096c8e2c0a3fb548bbb9e9ed
Alfredo Mora <alfredo9712mt@gmail.com>
2024-06-13 08:03:04 -0700

    finish project-one and project-two in progress

--- project-one/index.js
+++ project-one/index.js
@@ -1,1 +1,52 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 3;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.1;
+
+const scene = new THREE.Scene();
+
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshStandardMaterial({
+  color: 0x0ffffff,
+  flatShading: true,
+});
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const wireMat = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.01);
+scene.add(wireMesh);
+mesh.add(wireMesh);
+
+const hemiLight = new THREE.DirectionalLight(0xffffcc, 0.5);
+scene.add(hemiLight);
+
+const animate = (t = 0) => {
+  requestAnimationFrame(animate);
+  mesh.rotation.y = t * 0.0001;
+  mesh.rotation.x = t * 0.0001;
+  mesh.position.y = Math.sin(t * 0.001);
+  renderer.render(scene, camera);
+  controls.update();
+};
+animate();
